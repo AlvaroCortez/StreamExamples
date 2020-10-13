8c993c8e478a574680f4dd669d80526ffebb9f69
dmaslakov <maslakov_denis@mail.ru>
2020-10-14 01:11:41 +0300

    add map, all match, any match, none match examples

--- src/main/java/org/examples/stream/CodeExamples.java
+++ src/main/java/org/examples/stream/CodeExamples.java
@@ -4,12 +4,17 @@ import java.util.HashMap;
 
 public class CodeExamples {
     //will not properly work with ... parameters
+    //and with same count of arguments but different types
     public static final HashMap<String, String> classToFileMap = new HashMap<>() {
         {
             put("java.util.stream.Collectors.groupingBy1", "/examples/Collectors/Collectors.groupingBy.html");
             put("java.util.stream.Collectors.groupingBy2", "/examples/Collectors/Collectors.groupingBy.html");
             put("java.util.stream.Collectors.groupingBy3", "/examples/Collectors/Collectors.groupingBy.html");
             put("java.util.stream.Stream.flatMap1", "/examples/Stream/Stream.flatMap1.html");
+            put("java.util.stream.Stream.map1", "/examples/Stream/Stream.map1.html");
+            put("java.util.stream.Stream.allMatch1", "/examples/Stream/Stream.allMatch1.html");
+            put("java.util.stream.Stream.anyMatch1", "/examples/Stream/Stream.anyMatch1.html");
+            put("java.util.stream.Stream.noneMatch1", "/examples/Stream/Stream.noneMatch1.html");
         }
     };
 }
