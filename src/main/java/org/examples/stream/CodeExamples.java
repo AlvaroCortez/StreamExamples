a1311e44acad2324c480d9191622a8cd1131cd80
dmaslakov <maslakov_denis@mail.ru>
2020-10-13 23:39:31 +0300

    create class directories

--- src/main/java/org/examples/stream/CodeExamples.java
+++ src/main/java/org/examples/stream/CodeExamples.java
@@ -6,11 +6,10 @@ public class CodeExamples {
     //will not properly work with ... parameters
     public static final HashMap<String, String> classToFileMap = new HashMap<>() {
         {
-            put("java.util.stream.Collectors.groupingBy1", "/examples/Collectors.groupingBy.html");
-            put("java.util.stream.Collectors.groupingBy2", "/examples/Collectors.groupingBy.html");
-            put("java.util.stream.Collectors.groupingBy3", "/examples/Collectors.groupingBy.html");
-            put("org.springframework.boot.SpringApplication.run2", "/examples/SpringApplication.run.html");
-            put("java.util.stream.Stream.flatMap1", "/examples/Stream.flatMap1.html");
+            put("java.util.stream.Collectors.groupingBy1", "/examples/Collectors/Collectors.groupingBy.html");
+            put("java.util.stream.Collectors.groupingBy2", "/examples/Collectors/Collectors.groupingBy.html");
+            put("java.util.stream.Collectors.groupingBy3", "/examples/Collectors/Collectors.groupingBy.html");
+            put("java.util.stream.Stream.flatMap1", "/examples/Stream/Stream.flatMap1.html");
         }
     };
 }
